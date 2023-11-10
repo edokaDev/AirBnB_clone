394ed0e32cdeeb160262c08d74459aae6eba1243
Kyalo460 <kyalo460@gmail.com>
2023-11-10 21:45:46 +0300

    Changed how kwargs are copied to attributes

--- tests/test_save_reload_base_model.py
+++ tests/test_save_reload_base_model.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python3
-from models import storage
 from models.base_model import BaseModel
 
-all_objs = storage.all()
-print("-- Reloaded objects --")
-print()
-
-for obj_id in all_objs.keys():
-    obj = all_objs[obj_id]
-    print(obj)
-print()
-
-print("-- Create a new object --")
 my_model = BaseModel()
 my_model.name = "My_First_Model"
 my_model.my_number = 89
